00c43390b2162a002fec869047f6a830531d927d
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-03 10:16:46 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import { ChangeEvent } from "react"
-
+"use client"
+import { ChangeEvent, useState } from "react"
 
 export default function home(){
-  return <div className="flex justify-center items-center bg-black min-h-screen text-white">
+  const [ userId, setUserId ] = useState<string | any>();
+  return <div className="flex justify-center items-center bg-black min-h-screen text-gray-200">
     <div className="rounded-lg shadow-lg w-96 bg-gray-900">
       <div className="flex flex-col justify-center items-center font-lightweight text-xl p-5">
           Login to kite
@@ -11,7 +12,7 @@ export default function home(){
           <Input
             placeholder="Phone or User ID"
             type="text"
-            onChange={(e) => e.target.value}
+            onChange={(e) => setUserId(e.target.value)}
           />
           </div>
     </div>
@@ -25,9 +26,13 @@ interface LoginProps{
 }
 
 export function Input({ placeholder, type, onChange}: LoginProps){
-  return <div className="flex justify-center items-center"> 
-    <div className="flex flex-col w-80 rounded-md border border-gray-700 text-md text-gray-500 px-4 py-2 justify-center items-center">
-      { placeholder }
-    </div>
+  return <div className="flex justify-center items-center">
+    <input
+      className="flex flex-col w-80 rounded-md border border-gray-700 text-md text-gray-500 px-4 py-2 justify-center items-center"
+      placeholder={ placeholder }
+      type={type}
+      onChange={onChange}  
+    >
+    </input>
   </div>
 }
