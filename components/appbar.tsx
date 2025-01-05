84e392868cb623b250ad658b519b51c62331f633
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-04 23:17:55 -0500

    Update appbar.tsx

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -1,7 +1,32 @@
+'use client'
+import { useState } from "react"
 
 
 export default function AppBar(){
-    return <div className = "">
+    const [ niftyPrice, SetniftyPrice ] = useState<number | any>(24004.75);
+    const [ sensexPrice, SetSensexPrice ] = useState<number | any>(79223.11);
+    return <div className = "border border-b shadow-xl p-4 flex bg-black text-white p-5">
+        <div className="flex text-sm justify-content absolute left-0 top-3 font-semibold">
+            <div className="flex ml-32">
+                <div>
+                    Nifty 50 
+                </div>
+                <div className="ml-3 text-red-500">
+                    { niftyPrice }
+                </div>
+            </div>
+            <div className="flex ml-10">
+                <div>
+                    Sensex 
+                </div>
+                <div className="ml-3 text-red-500">
+                    { sensexPrice }
+                </div>
+            </div>
+        </div>
+        
+        <div>
 
+        </div>
     </div>
 }
