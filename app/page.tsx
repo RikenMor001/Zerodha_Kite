924f68d30c8e75191d5cd2ff2248e090f98186b9
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-04 21:17:35 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 
 import { ChangeEvent, useState } from "react";
 import Image from "next/image";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/16/solid";
 
 export default function Home() {
-  const [userId, setUserId] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [errors, setErrors] = useState<{ userId: string; password: string }>({
+  const [ userId, setUserId ] = useState<string>("");
+  const [ password, setPassword ] = useState<string>("");
+  const [ togglePassword, setTogglePassword ] = useState<boolean>(false);
+  const [ errors, setErrors ] = useState<{ userId: string; password: string }>({
     userId: "",
     password: "",
   });
@@ -36,13 +38,19 @@ export default function Home() {
             value={userId}
             onChange={(e) => setUserId(e.target.value)}
             error={errors.userId}
+
           />
+
           <Input
             placeholder="Password"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             error={errors.password}
+            showToggle
+            toggleVisibility={() => setTogglePassword((prev) => !prev)}
+            isVisible = {togglePassword}
+            
           />
         </div>
         <div className="flex justify-center items-center p-4">
@@ -54,7 +62,7 @@ export default function Home() {
           </button>
         </div>
         <div className="flex justify-center items-center text-sm text-gray-500 hover:text-orange-600 mb-6">
-          <button>Forgot user ID or password?</button>
+          <button> Forgot user ID or password? </button>
         </div>
         <div className="flex justify-center items-center p-2">
           <Image
@@ -84,13 +92,25 @@ interface LoginProps {
   value: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   error?: string;
+  showToggle?: boolean;
+  isVisible?: boolean;
+  toggleVisibility?: () => void
 }
 
-export function Input({ placeholder, type, value, onChange, error }: LoginProps) {
+export function Input({ 
+  placeholder, 
+  type, 
+  value, 
+  onChange, 
+  error, 
+  showToggle, 
+  isVisible, 
+  toggleVisibility}: LoginProps) {
   return (
     <div className="flex flex-col justify-center items-center p-3">
+      <div className="relative w-80">
       <input
-        className={`flex w-80 rounded-md border text-md px-5 py-3 bg-gray-900 justify-center items-center shadow-lg ${
+        className={`flex w-full rounded-md border text-md px-5 py-3 bg-gray-900 justify-center items-center shadow-lg ${
           error ? "border-red-500" : "border-gray-700"
         }`}
         placeholder={placeholder}
@@ -98,8 +118,23 @@ export function Input({ placeholder, type, value, onChange, error }: LoginProps)
         value={value}
         onChange={onChange}
       />
+
+      {showToggle && (
+        <button
+        type = 'button'
+        onClick={toggleVisibility}
+        className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-300"
+        >
+          { isVisible ? (
+            <EyeSlashIcon className="h-5 w-5"/>
+          ): (
+            <EyeIcon className="h-5 w-5"/>
+          )}
+        </button>
+      )}
       {error && <span className="text-red-500 text-sm mt-1">{error}</span>}
-    </div>
+      </div>
+      </div>
+
   );
 }
-
