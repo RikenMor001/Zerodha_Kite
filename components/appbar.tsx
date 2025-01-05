ec91340561305e844ed2019ea18d6edea29699f1
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-04 23:31:51 -0500

    Update appbar.tsx

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -3,24 +3,33 @@ import { useState } from "react"
 
 
 export default function AppBar(){
-    const [ niftyPrice, SetniftyPrice ] = useState<number | any>(24004.75);
-    const [ sensexPrice, SetSensexPrice ] = useState<number | any>(79223.11);
-    return <div className = "border border-b shadow-xl p-4 flex bg-black text-white p-5">
+    const [ niftyPrice, setniftyPrice ] = useState<number | any>(24004.75);
+    const [ sensexPrice, setSensexPrice ] = useState<number | any>(79223.11);
+    const [ numberChangeNifty, setNumberChangeNifty ] = useState<number | any>(-183.90);
+    const [ numberChangeSensex, setnumberChangeSensex ] = useState<number | any>(-720.60);
+
+    return <div className = "border border-b shadow-xl p-4 flex bg-black p-5">
         <div className="flex text-sm justify-content absolute left-0 top-3 font-semibold">
             <div className="flex ml-32">
-                <div>
+                <div className="text-white">
                     Nifty 50 
                 </div>
                 <div className="ml-3 text-red-500">
                     { niftyPrice }
                 </div>
+                <div className="text-gray-600 text-xs ml-2">
+                    {numberChangeNifty}
+                </div>
             </div>
-            <div className="flex ml-10">
-                <div>
+            <div className="flex ml-10 ">
+                <div className="text-white">
                     Sensex 
                 </div>
                 <div className="ml-3 text-red-500">
                     { sensexPrice }
+                </div>
+                <div className="text-gray-600 text-xs ml-2">
+                    { numberChangeSensex }
                 </div>
             </div>
         </div>
