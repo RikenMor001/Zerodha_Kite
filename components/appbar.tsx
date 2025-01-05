1ff777dab4dda16d4a377a9154ef58b135e4e5b9
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-05 15:04:33 -0500

    Update appbar.tsx

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -1,41 +1,67 @@
-'use client'
-import { useState } from "react"
+'use client';
+import { useState } from 'react';
 
+export default function AppBar() {
+  const [niftyPrice] = useState<number>(24004.75);
+  const [sensexPrice] = useState<number>(79223.11);
+  const [numberChangeNifty] = useState<number>(-183.9);
+  const [numberChangeSensex] = useState<number>(-720.6);
+  const userId = "HPD412";
+  const userInitial = userId[0];
 
-export default function AppBar(){
-    const [ niftyPrice, setniftyPrice ] = useState<number | any>(24004.75);
-    const [ sensexPrice, setSensexPrice ] = useState<number | any>(79223.11);
-    const [ numberChangeNifty, setNumberChangeNifty ] = useState<number | any>(-183.90);
-    const [ numberChangeSensex, setnumberChangeSensex ] = useState<number | any>(-720.60);
-
-    return <div className = "border border-b shadow-xl p-4 flex bg-black p-5">
-        <div className="flex text-sm justify-content absolute left-0 top-3 font-semibold">
-            <div className="flex ml-32">
-                <div className="text-white">
-                    Nifty 50 
-                </div>
-                <div className="ml-3 text-red-500">
-                    { niftyPrice }
-                </div>
-                <div className="text-gray-600 text-xs ml-2">
-                    {numberChangeNifty}
-                </div>
-            </div>
-            <div className="flex ml-10 ">
-                <div className="text-white">
-                    Sensex 
-                </div>
-                <div className="ml-3 text-red-500">
-                    { sensexPrice }
-                </div>
-                <div className="text-gray-600 text-xs ml-2">
-                    { numberChangeSensex }
-                </div>
-            </div>
+  return (
+    <div className="border-b shadow-xl p-6 flex bg-neutral-900 border-b-slate-800 justify-between items-center">
+      <div className="flex text-sm font-semibold relative">
+        <div className="flex space-x-3 ml-32">
+          <div className="text-white">
+            <button aria-label="NIFTY 50">
+              NIFTY 50
+            </button>
+          </div>
+          <div className="text-red-500">
+            {niftyPrice}
+          </div>
+          <div className="text-gray-600 text-xs">
+            {numberChangeNifty} (-0.76%)
+          </div>
         </div>
-        
+        <div className="flex space-x-3 ml-10">
+          <div className="text-white">
+            <button aria-label="SENSEX">
+              SENSEX
+            </button>
+          </div>
+          <div className="text-red-500">
+            {sensexPrice}
+          </div>
+          <div className="text-gray-600 text-xs">
+            {numberChangeSensex} (-0.90%)
+          </div>
+        </div>
+        <div className="border-l text-white ml-2 shadow-xl border-slate-800 flex space-x-9">
+          <div className="text-blue-500 ml-20">
+            Zerodha
+          </div>
+          <div className="flex space-x-9">
+            <button aria-label="Dashboard">Dashboard</button>
+            <button aria-label="Orders">Orders</button>
+            <button aria-label="Holdings">Holdings</button>
+            <button aria-label="Positions">Positions</button>
+            <button aria-label="Bids">Bids</button>
+            <button aria-label="Funds">Funds</button>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2 text-white">
+        <div className="bg-orange-500 rounded-full w-8 h-8 flex items-center justify-center text-lg">
+          { userInitial }
+        </div>
         <div>
-
+            <button>
+                { userId }
+            </button>
         </div>
+      </div>
     </div>
+  );
 }
