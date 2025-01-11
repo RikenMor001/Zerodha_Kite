cff5f467f7eac1c02e85f2aad622dbffabdbe837
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-11 18:36:28 -0500

    Add files via upload

---
+++ api/signup/route.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+  const { email, password } = await req.json();
+
+  const existingUser = await prisma.user.findUnique({
+    where: { email: email }
+  });
+
+  if (existingUser) {
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await prisma.user.create({
+    data: {
+      email: email,
+      password: hashedPassword
+    }
+  });
+
+  return NextResponse.json({
+    user: newUser.email,
+    message: "User created successfully"
+  }, { status: 201 });
+}
