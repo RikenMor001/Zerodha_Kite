fe889fb1f54ff145eea037964f6fdeb10c020205
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-11 18:35:43 -0500

    Update dashboard.tsx

--- dashboard/dashboard.tsx
+++ dashboard/dashboard.tsx
@@ -1,11 +1,1 @@
 
-import AppBar from "../components/appbar";
-
-export default function dashboard(){
-    return <div>
-        <AppBar/>
-        <div className="flex justify-center items-center min-h-screen bg-neutral-900 text-white">
-            Dashboard coming up soon...
-        </div>
-    </div>
-}
