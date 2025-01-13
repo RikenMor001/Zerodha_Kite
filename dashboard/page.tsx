e9e14b5e7f170e90b567917fdc864a8f0715ec0c
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-12 20:38:47 -0500

    Update page.tsx

--- dashboard/page.tsx
+++ dashboard/page.tsx
@@ -6,12 +6,12 @@ export default function dashboard(){
     return <div>
         <AppBar/>
         <div className="min-h-screen bg-neutral-900">
-            <div className="grid grid-cols-[35%_65%] gap-4 p-4 text-white">
+            <div className="grid grid-cols-[35%_65%] gap-4 p-4">
                 <div className="border-r border-neutral-800">
                     <SearchBar/>
                 </div>
-                <div className="text-white">
-                    hello there 
+                <div className="text-white ml-5 font-semibold text-2xl">
+                    Dashboard
                 </div>
             </div>
         </div>
