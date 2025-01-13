638de1edf8bc16dd7595d4ba17bb7c6920b858c6
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-13 09:25:08 -0500

    Update recommendations.tsx

--- components/recommendations.tsx
+++ components/recommendations.tsx
@@ -1,6 +1,30 @@
+// make a function 
+// no need of making any interface, cause I dont have any specific data types to impose
 
 export default function Recommendations(){
-    return <div className="text-white ml-5 font-semibold text-md mt-3">
-        Indix vix            
+    return <div className="text-white ml-5 font-semibold text-md mt-3 text-xs mt-5">
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
+                INDIA VIX           
+            </div>
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
+                NIFTY 50
+            </div>
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
+                HDFCBANK 
+            </div>
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
+                ICICIBANK 
+            </div>
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
+                KOTAKBANK 
+            </div>
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
+                AXISBANK 
+            </div>
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
+                SBIN 
+            </div>
     </div>    
 }
+  
+
