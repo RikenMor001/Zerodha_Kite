f5d5cc2455b682e5cd80b9f694ead223ad1cd56c
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-21 09:53:13 -0500

    Update recommendations.tsx

--- components/recommendations.tsx
+++ components/recommendations.tsx
@@ -1,30 +1,42 @@
-// make a function 
-// no need of making any interface, cause I dont have any specific data types to impose
 
 export default function Recommendations(){
-    return <div className="text-white ml-5 font-semibold text-md mt-3 text-xs mt-5">
-            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
-                INDIA VIX           
+    return <div className="text-white font-semibold text-xs mt-5">
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "13px", fontWeight: "lighter"}}>
+                <button>
+                    INDIA VIX           
+                </button>
             </div>
-            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
-                NIFTY 50
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "13px", fontWeight: "lighter"}}>
+                <button>
+                    NIFTY 50
+                </button>
             </div>
-            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
-                HDFCBANK 
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "13px", fontWeight: "lighter"}}>
+                <button>
+                    HDFCBANK 
+                </button>
             </div>
-            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
-                ICICIBANK 
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "13px", fontWeight: "lighter"}}>
+                <button>
+                    ICICIBANK 
+                </button>
             </div>
-            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
-                KOTAKBANK 
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "13px", fontWeight: "lighter"}}>
+                <button>
+                    KOTAKBANK 
+                </button>
             </div>
-            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
-                AXISBANK 
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "13px", fontWeight: "lighter"}}>
+                <button>
+                    AXISBANK 
+                </button>
             </div>
-            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "15px"}}>
-                SBIN 
+            <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "13px", fontWeight: "lighter"}}>
+                <button>
+                    SBIN
+                </button>
             </div>
     </div>    
 }
-  
+
 
