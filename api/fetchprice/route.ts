08a1be144bc606c256fe5fa202e667b5d155a1fa
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-22 23:13:34 -0500

    Update route.ts

--- api/fetchprice/route.ts
+++ api/fetchprice/route.ts
@@ -1,20 +1,63 @@
 import axios from "axios";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// use useEffect in the frontend code and also after i get the api key than i should be using web sockets 
 
-export default async function POST(req:NextRequest, res: NextResponse){
+const ALPHA_VANTAGE_API_KEY = process.env.ALPHA_VENTAGE_API_KEY || "demo";
+
+const stocks = [
+    {
+        symbol: "NIFTY50.BSE",
+        name: "Nifty 50"
+    },
+    {
+        symbol: "HDFCBANK.BSE",
+        name: "Hdfc bank"
+    },{
+        symbol: "ICICIBANK.BSE",
+        name: "Icici bank"
+    },{
+        symbol: "KOTAKBANK.BSE",
+        name: "Kotak bank"
+    },{
+        symbol: "AXISBANK.BSE",
+        name: "Axis bank"
+    },{
+        symbol: "SBIN.BSE",
+        name: "Sbi"
+    },
+]
+
+export async function GET(){
     try {
-        const response = await axios.get("//your_api_key");
-        const data = await response.data;
-        if (data.status === 200){
-            NextResponse.json({
-                message: "Data fetched successfully"
+        const stockData = await Promise.all(
+            stocks.map(async (stock) => {
+                try {
+                    const response = await axios.get(`https://www/alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock.symbol}&apikey=${ALPHA_VANTAGE_API_KEY}`);
+                    const quote = response.data["Global Quote"];
+                    return {
+                        symbol: stock.symbol,
+                        name: stock.name,
+                        price: quote?.['05. price'] || 'N/A',
+                        change: quote?.['09. change'] || 'N/A',
+                        changePercent: quote?.['10. change percent'] || 'N/A'
+                    }
+                }catch(error: any){
+                    console.error(`Error fetching ${stock.symbol}:` + error);
+                    return {
+                        symbol: stock.symbol,
+                        name: stock.name,
+                        price: "N/A",
+                        change: "N/A",
+                        changePercent:"N/A" 
+                    }
+                }
             })
-            return data;
-        }
-    }catch(error:any){
-        throw new Error("The error is:- " + error)
+        );
+        return NextResponse.json(stockData);
+    } catch(error: any){
+        return NextResponse.json({
+            error: "Failed to fetch the stock price"
+        }, {status: 500})
     }
     
 }
