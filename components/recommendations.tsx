0b7dfdc1c36f4f09057a28e2e9b27931fdee4c2f
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-23 20:34:23 -0500

    Update recommendations.tsx

--- components/recommendations.tsx
+++ components/recommendations.tsx
@@ -1,5 +1,23 @@
 
+interface StockData{
+    symbol: string,
+    price: number,
+    change: number,
+    changePercent: number
+}
+
 export default function Recommendations(){
+
+    const [ selectedStock, setSelectedStock ] = useState<string | null>(null);
+    const stocks: StockData[] = [
+        {
+            symbol: "INDIA VIX", 
+            price: 12.45,
+            change: 0.45,
+            changePercent: 3.75
+        }
+    ]
+
     return <div className="text-white font-semibold text-xs mt-5">
             <div className="mb-2 rounded-md border border-neutral-800 shadow=lg w-[90%] px-2 py-3" style={{fontSize: "13px", fontWeight: "lighter"}}>
                 <button>
