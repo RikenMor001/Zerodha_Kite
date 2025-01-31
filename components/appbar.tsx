6d319e6ccbdb36391f98dc6095c0ab64a00d2f53
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-30 23:52:58 -0500

    Update appbar.tsx

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
@@ -18,7 +19,7 @@ export default function AppBar() {
   const takeUserToOrders = () => router.push("/dashboard/orders");
   const takeUserToPositions = () => router.push("/dashboard/positions");
   const takeUserToHolding = () => router.push("/dashboard/holdings");
-  const handleLogout = () => alert("Logging out...");
+  const takeUserToSignInPage = () => router.push("/signin")
 
   return (
     <div className="border-b shadow-4xl p-3 flex bg-neutral-900 border-b-neutral-800 justify-between items-center">
@@ -49,15 +50,20 @@ export default function AppBar() {
       </div>
       <div className="relative flex flex-col items-center space-x-2 text-white">
         <div className="flex items-center space-x-2">
-          <div className="bg-orange-500 rounded-full w-6 h-6 flex items-center justify-center text-sm">{userInitial}</div>
-          <button className="text-sm hover:text-orange-500 hover:underline" onClick={() => setDropdownOpen(!dropdownOpen)}>{userId}</button>
+          <div className="bg-orange-500 rounded-full w-6 h-6 flex items-center justify-center text-sm">
+            {userInitial}
+          </div>
+          <button className="text-sm hover:text-orange-500 hover:underline" onClick={() => setDropdownOpen(!dropdownOpen)}>
+            {userId}
+          </button>
         </div>
         {dropdownOpen && (
           <div className="absolute top-full mt-2 w-32 bg-neutral-800 text-white shadow-lg rounded-md py-2 z-10">
-            <button className="block px-4 py-2 hover:bg-neutral-700 w-full text-left" onClick={handleLogout}>Logout</button>
+            <button className="px-4 py-2 hover:bg-neutral-700 w-full text-left" onClick={ takeUserToSignInPage }>Logout</button>
           </div>
         )}
       </div>
     </div>
   );
 }
+
