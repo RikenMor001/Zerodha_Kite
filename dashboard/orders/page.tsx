48c1b6013797f813d30d00c12c2e507ca161b832
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-03-06 11:56:14 -0500

    Update page.tsx

--- dashboard/orders/page.tsx
+++ dashboard/orders/page.tsx
@@ -53,7 +53,7 @@ export const Dropdown = ({type, placeholder, onChange}: DropDownStocksProps) => 
         <input 
         type={ type } 
         placeholder={ placeholder }
-        className="border border-neutral-800 rounded-md py-3 px-3 mt-3 w-full bg-neutral-800 text-neutral-300" 
+        className="border border-neutral-700 rounded-md py-3 px-7 mt-3 w-full bg-neutral-800 text-neutral-300 text-center shadow-lg placeholder-neutral-100 " 
         onChange={ onChange }
         >    
         </input>
