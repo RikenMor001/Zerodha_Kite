f6dcf0b1d48e558a7dc14aa7507cc2937eb41d6f
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-10 23:59:33 -0400

    Update page.tsx

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
-import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import Input from "../components/Input";
 
 export default function Home() {
   const [userId, setUserId] = useState<string>("");
@@ -15,7 +15,7 @@ export default function Home() {
     password: "",
   });
   const [message, setMessage] = useState<string>("");
-  const userIdName = "RC"
+  const userIdName = "RC";
   const router = useRouter();
 
   const handleSignup = async () => {
@@ -45,24 +45,22 @@ export default function Home() {
   };
 
   const takeUserToSignin = () => {
-    router.push("/signin")
-  }
+    router.push("/signin");
+  };
 
   const takeUserToSignup = () => {
-    router.push("/signup")
-  }
+    router.push("/signup");
+  };
 
   return (
     <div className="flex justify-center items-center bg-neutral-900 min-h-screen text-gray-200">
       <div className="rounded-lg shadow-xl w-96 bg-neutral-900 border border-neutral-800">
         <div className="flex flex-col justify-center items-center font-light text-2xl p-6 text-gray-200">
-        <div className="flex items-center space-x-2 text-white">
-        <div className="bg-orange-500 rounded-full w-16 h-16 flex items-center justify-center text-lg mb-3">
-          <button onClick={ takeUserToSignin }>
-            { userIdName }
-          </button>
-        </div>
-      </div>
+          <div className="flex items-center space-x-2 text-white">
+            <div className="bg-orange-500 rounded-full w-16 h-16 flex items-center justify-center text-lg mb-3">
+              <button onClick={takeUserToSignin}>{userIdName}</button>
+            </div>
+          </div>
           Signin
         </div>
         <div>
@@ -84,7 +82,6 @@ export default function Home() {
             toggleVisibility={() => setTogglePassword((prev) => !prev)}
             isVisible={togglePassword}
           />
-          
         </div>
         <div className="flex justify-center items-center p-4">
           <button
@@ -94,19 +91,15 @@ export default function Home() {
             Signin
           </button>
         </div>
-          <div className="flex justify-center items-center text-sm ">
-            <div className="text-red-500">
-              Haven't signed up yet? 
-            </div>
-            <div className="text-red-500 hover:text-red-600 ml-1 cursor-pointer">
-              <button onClick={takeUserToSignup}>
-                Signup
-              </button>
-            </div>
+        <div className="flex justify-center items-center text-sm ">
+          <div className="text-red-500">Haven't signed up yet?</div>
+          <div className="text-red-500 hover:text-red-600 ml-1 cursor-pointer">
+            <button onClick={takeUserToSignup}>Signup</button>
           </div>
+        </div>
         {message && (
           <div className="flex justify-center items-center text-sm text-red-500 mb-6">
-            { message }
+            {message}
           </div>
         )}
         <div className="flex justify-center items-center p-4">
@@ -122,57 +115,3 @@ export default function Home() {
     </div>
   );
 }
-
-interface LoginProps {
-  placeholder: string;
-  type: string;
-  value: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  error?: string;
-  showToggle?: boolean;
-  isVisible?: boolean;
-  toggleVisibility?: () => void;
-}
-
-export function Input({
-  placeholder,
-  type,
-  value,
-  onChange,
-  error,
-  showToggle,
-  isVisible,
-  toggleVisibility,
-}: LoginProps) {
-  return (
-    <div className="flex flex-col justify-center items-center p-3">
-      <div className="relative w-80">
-        <input
-          className={`flex w-full rounded-md border text-md px-5 py-3 bg-neutral-900 justify-center items-center shadow-lg ${
-            error ? "border-red-500" : "border-neutral-800"
-          }`}
-          placeholder={placeholder}
-          type={type}
-          value={value}
-          onChange={onChange}
-        />
-
-        {showToggle && (
-          <button
-            type="button"
-            onClick={toggleVisibility}
-            className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-300"
-          >
-            {isVisible ? (
-              <EyeSlashIcon className="h-5 w-5" />
-            ) : (
-              <EyeIcon className="h-5 w-5" />
-            )}
-          </button>
-        )}
-        {error && <span className="text-red-500 text-sm mt-1">{error}</span>}
-      </div>
-    </div>
-  );
-}
-
