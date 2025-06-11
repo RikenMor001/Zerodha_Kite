6a410d31bcf33c4d1cf163b129793e9fc990f12a
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-11 10:43:39 -0400

    Update route.ts

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -6,32 +6,36 @@ const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   try {
-  const { email, password } = await req.json();
+    const { email, password } = await req.json();
 
-  const existingUser = await prisma.user.findUnique({
-    where: { email: email }
-  });
+    const existingUser = await prisma.user.findUnique({
+      where: { email }
+    });
 
-  if ( existingUser ) {
-    return NextResponse.json({ error: "User already exists" }, { status: 400 });
-  }
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const newUser = await prisma.user.create({
-    data: {
-      email: email,
-      password: hashedPassword
+    if (existingUser) {
+      return NextResponse.json({ error: "User already exists" }, { status: 400 });
     }
-  });
 
-  return NextResponse.json({
-    user: newUser.email,
-    message: "User created",
-    alert: "User Signed up!"
-  }, { status: 200 });
-} catch(error: any){
-  return NextResponse.json({
-    error: error.message
-  })
-}
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword
+      }
+    });
+
+    return NextResponse.json({
+      user: newUser.email,
+      message: "User created",
+      alert: "User Signed up!"
+    }, { status: 200 });
+
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    return NextResponse.json({ error: "An unknown error occurred" }, { status: 500 });
+  }
 }
