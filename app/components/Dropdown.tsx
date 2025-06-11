49a9a8121b6186d2a7a9d57b6956d38face55b96
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-10 23:40:05 -0400

    Add files via upload

---
+++ app/components/Dropdown.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+interface DropDownStocksProps {
+  type: string;
+  placeholder: string;
+  onChange: (e: any) => void;
+}
+
+const Dropdown = ({ type, placeholder, onChange }: DropDownStocksProps) => {
+  return (
+    <div>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="border border-neutral-700 rounded-md py-3 px-7 mt-3 w-full bg-neutral-800 text-neutral-300 text-center shadow-lg placeholder-neutral-100"
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default Dropdown;
