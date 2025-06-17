e7d296bb17255ac8724ba58d13e91649dd0b3ec6
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-17 18:42:20 -0400

    Update WebSocketStockData.tsx

--- app/components/WebSocketStockData.tsx
+++ app/components/WebSocketStockData.tsx
@@ -1,16 +1,31 @@
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 // stockPrice, symbol, name, percentageChange, lastUpdated, change
 
 interface stockData{
-
+  stockPrice: string;
+  symbol: string;
+  name: string;
+  percentageChange: string;
+  lastUpdated: string;
+  change: string;
 }
 
 const WebSocketData: React.FC = () => {
-  const [stockData, setStockData] = useState([]);
+  const [stockData, setStockData] = useState<stockData[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [errors, setErrors] = useState<string | number | null>();
+
+  // It has to be an async function because there is a request 
+  // being sent to the backend route. 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      
+    }
+  }, [])
 
   return <div>
 
