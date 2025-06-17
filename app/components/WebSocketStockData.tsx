0c848f3ee0975df636bfb42313553d1577d1dbf8
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-17 19:44:53 -0400

    Update WebSocketStockData.tsx

--- app/components/WebSocketStockData.tsx
+++ app/components/WebSocketStockData.tsx
@@ -1,3 +1,4 @@
+
 import { useEffect, useState } from "react"
 
 // stockPrice, symbol, name, percentageChange, lastUpdated, change
@@ -20,28 +21,46 @@ const WebSocketData: React.FC = () => {
   // being sent to the backend route. 
 
   useEffect(() => {
-    try {
     const fetchData = async () => {
-      setLoading(true);
-      const response = await fetch('/api/stock');      
-      if (!response.ok){
-        throw new Error(`HTTP error! status: ${response.status}`);
+      try {
+        setLoading(true);
+        const response = await fetch('api/stock');
+        if (!response.ok){
+          throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const data = await response.json();
+        setStockData(data);
+        setError(null);
+
+      } catch(error){
+          console.error("Fetch request failed");
+          setError("Failed to fetch data. Please try again later!")
+      } finally{
+        setLoading(false)
       }
-      
-      const data = await response.json();
-      setStockData(data);
-      setError(null);
     }
-    } catch(error){
-      console.error('Error fetching datta' + error);
-      setError("Failed to fetch data. Please try again later!");
-    } finally {
-      setLoading(false)
-    }
+
+    fetchData();
+    // Hit the api route from the frontend and fetch data every 3 seconds
+    const interval = setInterval(fetchData, 3000);
+
+    return () => clearInterval(interval);
   }, [])
 
-  return <div>
+  
+    if (loading && stockData.length === 0){
+      return <div className="text-slate-400 font-semibold">
+        Loading stock data...
+      </div>  
+    }
 
+    if (error){   
+      return <div className="text-red-500 font-semibold">
+        {error}
+      </div>
+    }
+
+  return <div>
   </div>
 }
 
