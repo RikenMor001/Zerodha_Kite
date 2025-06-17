3de871755273cf1fd4eaf08ae9a16c246b0ba7ed
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-06-17 18:54:43 -0400

    Update WebSocketStockData.tsx

--- app/components/WebSocketStockData.tsx
+++ app/components/WebSocketStockData.tsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from "react"
 
 // stockPrice, symbol, name, percentageChange, lastUpdated, change
@@ -15,15 +14,29 @@ interface stockData{
 const WebSocketData: React.FC = () => {
   const [stockData, setStockData] = useState<stockData[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  const [errors, setErrors] = useState<string | number | null>();
+  const [error, setError] = useState<string | number | null>();
 
   // It has to be an async function because there is a request 
   // being sent to the backend route. 
 
   useEffect(() => {
+    try {
     const fetchData = async () => {
       setLoading(true);
+      const response = await fetch('/api/stock');      
+      if (!response.ok){
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
       
+      const data = await response.json();
+      setStockData(data);
+      setError(null);
+    }
+    } catch(error){
+      console.error('Error fetching datta' + error);
+      setError("Failed to fetch data. Please try again later!");
+    } finally {
+      setLoading(false)
     }
   }, [])
 
